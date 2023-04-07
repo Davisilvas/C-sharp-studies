f47ce1c6af158b5f16e260084c382b32aa893e7d
Davi Silva Santos <morg.daviss@gmail.com>
2023-04-06 21:13:41 -0300

    beginning of c# journey

---
+++ section_03/Basics/Basics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Basics // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o seu nome:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Informe quantos quartos há na sua casa:");
+            int rooms = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a altura da sua casa");
+            double houseHeight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nos informe o seu: último nome, sua idade e sua altura");
+            string[] arr = Console.ReadLine().Split();
+            string lastName = arr[0];
+            int age = int.Parse(arr[1]);
+            double userHeight = double.Parse(arr[2]);
+
+            Console.WriteLine("********************");
+            Console.WriteLine("Suas entradas foram: ");
+            Console.WriteLine(name);
+            Console.WriteLine(rooms);
+            Console.WriteLine(houseHeight.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(lastName);
+            Console.WriteLine(age);
+            Console.WriteLine(userHeight.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
