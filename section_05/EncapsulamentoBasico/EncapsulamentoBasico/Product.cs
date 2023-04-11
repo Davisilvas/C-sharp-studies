8cf88bb6f42c87d517debacd93a39ed9a4bc2650
Davi Silva Santos <morg.daviss@gmail.com>
2023-04-11 16:00:20 -0300

    Encapsulamento Básico

---
+++ section_05/EncapsulamentoBasico/EncapsulamentoBasico/Product.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace EncapsulamentoBasico
+{
+    internal class Product
+    {
+        private string _name;
+        private double _price;
+        private int _qtd;
+
+        public Product()
+        {
+         
+        }
+
+        public Product(string name, double price, int qtd)
+        {
+            _name = name;
+            _price = price;
+            _qtd = qtd;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void SetNome(string name)
+        {
+            _name = name;
+        }
+
+        public int GetQtd()
+        {
+            return _qtd;
+        }
+
+        public double GetPrice()
+        {
+            return _price;
+        }
+
+        public override string ToString()
+        {
+            return _name + " - $"
+                + _price.ToString("F2", CultureInfo.InvariantCulture)
+                + " - Quantidade em estoque: "
+                + _qtd + " - Soma: "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return _price * _qtd;
+        }
+
+        public void AddProduct(int quantity)
+        {
+            _qtd = _qtd + quantity;
+        }
+
+        public void RemoveProduct(int quantity)
+        {
+            _qtd = _qtd - quantity;
+        }
+
+    }
+}
---
+++ section_05/EncapsulamentoBasico/EncapsulamentoBasico/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace EncapsulamentoBasico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade de items no estoque: ");
+            int qtd = int.Parse(Console.ReadLine()); 
+
+            Product produto = new Product(name, price, qtd);
+
+            Console.WriteLine("_____________________");
+            Console.WriteLine(produto.GetName());
+            Console.WriteLine(produto.GetPrice());
+            Console.WriteLine(produto.GetQtd());
+
+            Console.WriteLine("_____________________");
+            Console.Write("Informe o novo nome: ");
+            produto.SetNome(Console.ReadLine());
+            Console.WriteLine("Nome atualizado: " + produto.GetName());
+        }
+    }
+}
