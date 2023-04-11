b851a61497c3d088e12dc770d4ed56f3e44755b7
Davi Silva Santos <morg.daviss@gmail.com>
2023-04-11 19:21:08 -0300

    Auto Properties

---
+++ section_05/AutoPropertie/AutoPropertie/Product.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Intrinsics.Arm;
+
+    /*
+    Ordem sugerida
+    • Atributos privados
+    • Propriedades autoimplementadas
+    • Construtores
+    • Propriedades customizadas
+    • Outros métodos da classe */
+
+namespace AutoPropertie
+{
+    internal class Product
+    {
+        // se o valor tem uma lógica particular como o _name, não da pra aplicar a auto prop. No caso de _name, ela tem uma lógica de validação.
+        
+        private string _name;
+        public int Qtd { get; private set; }
+        public double Price { get; private set; }
+
+
+
+        // CONSTRUCTORS
+        public Product()
+        {
+
+        }
+
+        public Product(string name, double price, int qtd)
+        {
+            _name = name;
+            Price = price;
+            Qtd = qtd;
+        }
+
+        // PROPERTIES
+        public string Name
+        {
+            get { return _name; }
+
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        // METHODS
+        public double ValorTotalEmEstoque()
+        {
+            return Price * Qtd;
+        }
+
+        public void AddProduct(int quantity)
+        {
+            Qtd = Qtd + quantity;
+        }
+
+        public void RemoveProduct(int quantity)
+        {
+            Qtd = Qtd - quantity;
+        }
+
+        public override string ToString()
+        {
+            return _name + " - $"
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + " - Quantidade em estoque: "
+                + Qtd + " - Soma: "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ section_05/AutoPropertie/AutoPropertie/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace AutoPropertie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade de items no estoque: ");
+            int qtd = int.Parse(Console.ReadLine());
+
+            Product produto = new Product(name, price, qtd);
+
+            Console.WriteLine("_____________________");
+            Console.WriteLine(produto.Name);
+            Console.WriteLine(produto.Price);
+            Console.WriteLine(produto.Qtd);
+
+            Console.WriteLine("_____________________");
+            Console.Write("Informe o novo nome: ");
+            produto.Name = Console.ReadLine();
+            Console.WriteLine("Nome atualizado: " + produto.Name);
+        }
+    }
+}
