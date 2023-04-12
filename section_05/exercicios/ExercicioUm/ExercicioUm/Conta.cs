44e0d6e892dc5a24c3abc19eca0fb2795c776811
Davi Silva Santos <morg.daviss@gmail.com>
2023-04-12 07:32:43 -0300

    exercicio finalizado com a minha solução.

---
+++ section_05/exercicios/ExercicioUm/ExercicioUm/Conta.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Diagnostics;
+
+namespace ExercicioUm
+{
+    internal class Conta
+    {
+        private int _accountNumber;
+        private string _name;
+        private double _bankDeposit;
+        public double BankBalance { get; private set; }
+
+        // CONSTRUCTORS
+        public Conta(string name, int accountNumber)
+        {
+            _name = name;
+            _accountNumber = accountNumber;
+        }
+
+        public Conta(string name, int accountNumber, double bankDeposit) : this(name, accountNumber)
+        {
+            _bankDeposit = bankDeposit;
+
+            Deposit(bankDeposit);
+        }
+
+
+
+
+        public string Name
+        {
+            get { return _name; }
+
+            set
+            {
+                if (value != null && value.Length >= 3)
+                {
+                    _name = value;
+                }
+                else
+                {
+                    Console.WriteLine("O nome não pode ser vazio e deve conter no mínimo 3 caracteres.");
+                }
+            }
+        }
+
+        public int AccountNumber
+        {
+            get { return _accountNumber; }
+            private set
+            {
+                if (value < 1000)
+                {
+                    Console.WriteLine("O Número da conta deve conter 4 dígito e não pode começar com 0.");
+                }
+                else
+                {
+                    _accountNumber = value;
+                }
+            }
+        }
+
+        public double BankDeposit
+        {
+            get { return _bankDeposit; }
+            private set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("O depósito inicial deve ser maior que R$0.");
+                }
+                else
+                {
+                    _bankDeposit = value;
+                }
+            }
+        }
+
+        // METHODS
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                BankBalance += amount;
+            }
+            else
+            {
+                Console.WriteLine("O Valor de um depósito deve ser maior do que 0.");
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > 0)
+            {
+                BankBalance -= 5;
+                BankBalance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("O Valor de um saque deve ser maior do que 0.");
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " + _accountNumber
+                + ", Titular da conta: " + _name
+                + ", Saldo total: " + BankBalance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ section_05/exercicios/ExercicioUm/ExercicioUm/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioUm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("****************************************");
+            Console.WriteLine("Seja bem vindo ao Banco Vasco da Gama");
+            Console.WriteLine("****************************************");
+
+            Conta account;
+
+            Console.WriteLine();
+            Console.Write("Entre o número da conta: ");
+            int AcNumber = int.Parse(Console.ReadLine()); // AcNumber = AccountNumber
+
+            Console.Write("Entre o titular da conta: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char decision = char.Parse(Console.ReadLine());           
+
+
+            if (decision == 's' || decision == 'S') 
+            {
+                Console.Write("Entre o valor do depósito inicial: ");
+                double EntryDeposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                account = new Conta(name, AcNumber, EntryDeposit);
+            }
+            else
+            {
+                account = new Conta(name, AcNumber);
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(account);
+            Console.WriteLine("****************************************");
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            account.Deposit(amount);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(account);
+            Console.WriteLine("****************************************");
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            account.Withdraw(amount);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(account);
+            Console.WriteLine("****************************************");
+
+        }
+    }
+}
