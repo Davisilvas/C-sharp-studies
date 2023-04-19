bed19795dd94a3b4ca0e727421135c2fb6d87bb1
Davi Silva Santos <morg.daviss@gmail.com>
2023-04-19 11:57:13 -0300

    exercicio solucionado

--- section_06/exercicios/ExFuncionario/ExFuncionario/Program.cs
+++ section_06/exercicios/ExFuncionario/ExFuncionario/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Globalization;
+using System;
 
 namespace ExFuncionario
 {
@@ -6,10 +7,54 @@ namespace ExFuncionario
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            int value = 2000;
-            double salary = value * 0.1;
-            Console.WriteLine(salary);
+            Console.Write("How many employees will be registered? ");
+            int numberOfEmployees = int.Parse(Console.ReadLine());
+
+            List<Employee> staff = new List<Employee>();
+
+            for (int i = 1; i <= numberOfEmployees; i++)
+            {
+                Console.WriteLine($"Employee #{i}:");
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                staff.Add(new Employee(id, name, salary));
+                Console.WriteLine();
+            }
+
+            Console.Write("Enter the employee id that will have salary increase: ");
+            int increaseID = int.Parse(Console.ReadLine());
+
+            Employee employeeId = staff.Find(x => x.ID == increaseID);
+
+            if (employeeId != null)
+            {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                employeeId.IncreaseSalary(percentage);
+            }
+            else
+            {
+                Console.WriteLine("This id does not exist");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees");
+            foreach (Employee employee in staff)
+            {
+                Console.WriteLine(employee);
+            }
+
+
+
+
+
+
         }
     }
 }
