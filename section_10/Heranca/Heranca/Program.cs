537e31814aad693e2cf056f4b52a95bd575a1b91
Davi Silva Santos <morg.daviss@gmail.com>
2023-04-25 11:24:00 -0300

    inheritance basics

---
+++ section_10/Heranca/Heranca/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Heranca.Entities;
+
+namespace Heranca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(2810, "Davi Santos", 100.00, 500.00);
+            
+            Console.WriteLine(account.Balance);           
+        }
+    }
+}
