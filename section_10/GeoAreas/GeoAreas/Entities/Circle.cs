7b4cf91c5ce727abc1b8b03c8704cee16324073b
Davi Silva Santos <morg.daviss@gmail.com>
2023-04-27 20:18:24 -0300

    end of section10

---
+++ section_10/GeoAreas/GeoAreas/Entities/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using GeoAreas.Entities.Enums;
+
+namespace GeoAreas.Entities
+{
+    internal class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius, Color color) : base(color)
+        {
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
---
+++ section_10/GeoAreas/GeoAreas/Entities/Shape.cs
@@ -0,0 +1,16 @@
+﻿using GeoAreas.Entities.Enums;
+
+namespace GeoAreas.Entities
+{
+    abstract class Shape
+    {
+        public Color Color { get; set; }
+
+        public Shape(Color color)
+        {
+            Color = color;
+        }
+
+        public abstract double Area();
+    }
+}
