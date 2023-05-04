46504dc27f3c2aefb26c83630f8a9d3e92d63980
Davi Silva Santos <morg.daviss@gmail.com>
2023-05-03 21:26:15 -0300

    half of chess game

---
+++ section_12/Chess/Chess/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Tabuleiro;
+
+namespace Chess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ section_12/Chess/Chess/Tela.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Tabuleiro;
+
+namespace Chess
+{
+    internal class Tela
+    {
+        public static void imprimirTabuleiro(Tabuleiro tab)
+        {
+
+        }
+    }
+}
