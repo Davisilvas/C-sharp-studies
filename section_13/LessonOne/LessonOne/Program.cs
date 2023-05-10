07a9563c48c8b03f8abe5398548d0b7edb9dea57
Davi Silva Santos <morg.daviss@gmail.com>
2023-05-10 11:18:00 -0300

    section 13 - 1st file lesson

---
+++ section_13/LessonOne/LessonOne/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace LessonOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"D:\MY_DESKTOP\RemediosDoly.txt";
+            string targetPah = @"D:\MY_DESKTOP\file1.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPah);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+                Console.WriteLine();
+                Console.WriteLine(lines[0]);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
