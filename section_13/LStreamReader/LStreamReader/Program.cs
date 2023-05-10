84b0ce1e0b1b3b5c40b6b3d808b611821bdbc0a8
Davi Silva Santos <morg.daviss@gmail.com>
2023-05-10 11:49:26 -0300

    section 13 - FileStream & StreamReader

---
+++ section_13/LStreamReader/LStreamReader/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace LStreamReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\MY_DESKTOP\RemediosDoly.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+               /*
+                fs = new FileStream(path, FileMode.Open);
+                // IMPORTANTE TER A REFERÊNCIA DO FILESTREAM PRA PODER FECHAR ELE NO FINAL
+                sr = new StreamReader(fs);
+               */
+               sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }                
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred: ");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();                
+            }
+        }
+    }
+}
