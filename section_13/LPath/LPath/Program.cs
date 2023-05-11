b576742458dc85c7a0b9668a9e688314c24b440c
Davi Silva Santos <morg.daviss@gmail.com>
2023-05-11 18:34:02 -0300

    Finished Files Section (13)

---
+++ section_13/LPath/LPath/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LPath
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\MY_DESKTOP\1 CODES\c_CSharpe\section_13\LDirectory\LessonFile\file1.txt";
+
+            Console.WriteLine("GetDirectoryName");
+            Console.WriteLine(Path.GetDirectoryName(path));
+            Console.WriteLine();
+
+
+            Console.WriteLine("DirectorySeparatorChar");
+            Console.WriteLine(Path.DirectorySeparatorChar);
+            Console.WriteLine();
+
+            Console.WriteLine("PathSeparator");
+            Console.WriteLine(Path.PathSeparator);
+            Console.WriteLine();
+
+            Console.WriteLine("GetFileName");
+            Console.WriteLine(Path.GetFileName(path));
+            Console.WriteLine();
+
+            Console.WriteLine("GetFileNameWithoutExtension");
+            Console.WriteLine(Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine();
+
+            Console.WriteLine("GetExtension");
+            Console.WriteLine(Path.GetExtension(path));
+            Console.WriteLine();
+
+            Console.WriteLine("GetFullPath");
+            Console.WriteLine(Path.GetFullPath(path));
+            Console.WriteLine();
+
+            Console.WriteLine("GetTempPath");
+            Console.WriteLine(Path.GetTempPath());
+            Console.WriteLine();
+        }
+    }
+}
---
+++ section_13/LStreamWriter/LStreamWriter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace LStreamWriter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"D:\MY_DESKTOP\RemediosDoly.txt";
+            string targetPath = @"D:\MY_DESKTOP\file2.txt";
+            
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+                using(StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach(string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
