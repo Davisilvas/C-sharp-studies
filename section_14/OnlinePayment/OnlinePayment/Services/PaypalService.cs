41e239d5361d4215c86b1c2f7e5ea652016c9d2a
Davi Silva Santos <morg.daviss@gmail.com>
2023-05-16 13:15:14 -0300

    finished contracts exercise

---
+++ section_14/OnlinePayment/OnlinePayment/Services/PaypalService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnlinePayment.Services
+{
+    internal class PaypalService : IOnlinePaymentService
+    {
+
+        private const double FeePercentage = 0.02;
+        private const double MonthlyInterest = 0.01;
+
+        double IOnlinePaymentService.Interest(double amount, int months)
+        {
+            return amount * MonthlyInterest * months;
+        }
+
+        double IOnlinePaymentService.PaymentFee(double amount)
+        {
+            return amount * FeePercentage;
+        }
+    }
+}
